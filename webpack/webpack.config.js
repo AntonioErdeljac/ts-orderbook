d749e80a396e046fca450291212218bf42af0d77
Antonio <hedhogs@gmail.com>
2021-12-15 22:47:52 +0100

    feat: add webpack envs

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,44 +1,11 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
 
-module.exports = {
-  entry: path.resolve(__dirname, '..', './src/index.tsx'),
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: 'asset/inline',
-      },
-    ],
-  },
-  output: {
-    path: path.resolve(__dirname, '..', './build'),
-    filename: 'bundle.js',
-  },
-  mode: 'development',
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: path.resolve(__dirname, '..', './src/index.html'),
-    }),
-  ],
-}
+const common = require('./webpack.common.js');
+
+module.exports = (options) => {
+  const { env } = options;
+  const mode = require(`./webpack.${env}.js`);
+  const config = merge(common, mode);
+
+  return config;
+};
