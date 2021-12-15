fa3e038d4efe21711f858ad684e811e111af827b
Antonio <hedhogs@gmail.com>
2021-12-15 23:31:12 +0100

    lint: fix formatting

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -3,7 +3,5 @@ const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
-  plugins: [
-    new BundleAnalyzerPlugin(),
-  ],
+  plugins: [new BundleAnalyzerPlugin()],
 };
